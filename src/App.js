2ff402dc7e478708d778622ac570b1ffc05670c5
Alicia Harner <alicia.harner07@gmail.com>
2024-05-31 14:43:35 +0800

    changed alt text to not include word "picture"

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import profilePhoto from './me.jpeg';
 function App() {
   return (
     <div class="app">
-      <img class="profile" alt="Picture of Author" src={profilePhoto}></img>
+      <img class="profile" alt="Author" src={profilePhoto}></img>
       <h1>Alicia Harner</h1>
       <p>Ba ba ba. Ba ba banana</p>
     </div>
